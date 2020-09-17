14d44cc05f977749163af0a3f0ab3d7305efcceb
DMHDMH00 <70082426+AndreDMH@users.noreply.github.com>
2020-09-17 18:00:17 +0800

    project

---
+++ d2-admin-start-kit/src/views/demo/page4/index.vue
@@ -0,0 +1,144 @@
+/*eslint-disable /
+<template>
+  <d2-container>
+    <template slot="header">这是一个测试录入系统</template>
+    <!-- 第一部分 -->
+    <div class="app">
+    <div class="header">
+    <div>
+      <span>姓名：</span>
+      <span>
+        <input type="text" placeholder="请输入姓名" v-model="newStudent.name">
+      </span>
+    </div>
+    <div>
+      <span>年龄：</span>
+      <span>
+        <input type="text" placeholder="请输入年龄" v-model="newStudent.age">
+      </span>
+    </div>
+    <div>
+      <span>性别：</span>
+      <select v-model="newStudent.sex">
+        <option value="男">男</option>
+        <option value="女">女</option>
+      </select>
+    </div>
+    <div>
+      <span>电话：</span>
+      <input type="tel" placeholder="请输入电话号码" v-model="newStudent.phone">
+    </div>
+    <div>
+    <input type="submit" value="提交" @click="createnewStudent()"/>
+    </div>
+    </div>
+    <!-- 第二部分 -->
+    <table>
+      <thead>
+        <tr>
+          <td>姓名</td>
+          <td>年龄</td>
+          <td>性别</td>
+          <td>电话</td>
+          <td>删除</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(p,index) in persons" :key="index">
+          <td>{{p.name}}</td>
+          <td>{{p.age}}</td>
+          <td>{{p.sex}}</td>
+          <td>{{p.phone}}</td>
+          <td>
+            <button @click="deleteStudentMsg()">删除</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+  </d2-container>
+</template>
+
+<script>
+export default {
+  name: 'page4',
+  data(){
+    return{
+      persons:[{
+        name:"aaa",
+        age:20,
+        sex:"男",
+        phone:"13831388998"
+      },{
+        name:"bbb",
+        age:21,
+        sex:"女",
+        phone:"13831388998"
+      },{
+        name:"ccc",
+        age:22,
+        phone:"13831388998",
+        sex:"男",
+      }
+      ],
+      newStudent:{
+        name:"",
+        age:"",
+        sex:"",
+        phone:""
+      }
+    }
+  },
+  methods:{
+    //创建一条新纪录
+    createnewStudent(){
+      if(this.newStudent.name===""){
+        alert("姓名不能为空");
+        return;
+      }
+      if(this.newStudent.age<=0||this.newStudent.age>=120){
+        alert("请输入正确的年龄");
+        return;
+      }
+      if(this.newStudent.phone===""){
+        alert("电话号码格式不正确");
+        return;
+      }
+      //往数组中添加一条数据
+      this.persons.unshift(this.newStudent);
+      //清空数据
+      this.newStudent = {
+        name:"",
+        age:0,
+        sex:"",
+        phone:""
+      }
+    },
+    //删除一条记录
+    deleteStudentMsg(index){
+      this.persons.splice(index,1);
+    }
+  }
+}
+</script>
+<style>
+  .app{
+    margin: auto;
+    width: 600px;
+  }
+  .header{
+    width: 597px;
+    margin-bottom: 20px;
+    border: solid blueviolet;
+
+  }
+  table {
+    width: 600px;
+    border: solid blueviolet;
+    border-collapse: collapse;
+    text-align: center;
+  }
+  thead{
+    background-color: cornflowerblue;
+  }
+</style>
---
+++ d2-admin-start-kit/src/views/demo/page5/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <d2-container>
+    <template slot="header">网站的头部</template>
+      <div>
+        <legend>简易视频网站</legend>
+        <!-- <header id="header"></header>
+        <nav id="nav"></nav>
+        <section id="section"></section>
+        <footer id="footer"></footer> -->
+
+        <div id="header">
+          <span>
+            <a href="#">电影</a>
+            <a href="#">电视剧</a>
+            <a href="#">动漫</a>
+            <a href="#">短视频</a>
+            <a href="#">游戏视频</a>
+            <a href="#">纪录片</a>
+            <a href="#">中央频道</a>
+          </span>
+        </div>
+        <div id="sec">
+          <tr>
+            <td><img src=""></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </div>
+        <div id="footer">
+          
+        </div>
+      </div>
+    <template slot="footer">网站的尾部</template>
+  </d2-container>
+</template>
+
+<script>
+export default {
+  name: 'page5'
+}
+</script>
+<style scoped>
+  #header{
+    width: 885.2px;
+    height: 20px;
+    background-color:rgb(170, 225, 247);
+    text-align: center;
+  }
+  a{
+    margin: 20px;
+  }
+  #sec{
+    width: 883.2px;
+    height: 480px;
+    border: ridge 1px palevioletred;
+  } 
+  #footer{
+    width: 885.2px;
+    height: 30px;
+    background-color: rgb(170, 225, 247);
+  }
+  td{
+    height: 160px;
+    width: 200px;
+    border: ridge paleturquoise 1px;
+  }
+</style>>
